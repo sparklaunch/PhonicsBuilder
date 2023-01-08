903ef37e4def55bd17e53fdaf4cb6f2f77d2e975
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-09 07:21:25 +0900

    Initial Commit

---
+++ PhonicsBuilder/PhonicsBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhonicsBuilderApp.swift
+//  PhonicsBuilder
+//
+//  Created by 신정훈 on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct PhonicsBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
