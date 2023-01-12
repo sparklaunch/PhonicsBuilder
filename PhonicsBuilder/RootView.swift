39f312d90ae98dd1a8dd1df9c0f0810427202d16
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-12 11:18:16 +0900

    [Fix] The weird black gap issue.

--- PhonicsBuilder/RootView.swift
+++ PhonicsBuilder/RootView.swift
@@ -3,13 +3,16 @@ import SwiftUI
 struct RootView: View {
     @EnvironmentObject var camera: Camera
     var body: some View {
-        ZStack {
-            Preview()
-                .rotationEffect(.degrees(-90))
-                .scaledToFill()
-            Text(camera.results.isEmpty ? "No Input" : camera.results.joined(separator: ", "))
-                .font(.largeTitle)
-                .foregroundColor(.white)
+        GeometryReader { geometry in
+            ZStack {
+                Preview()
+                    .rotationEffect(.degrees(-90))
+                    .scaledToFill()
+                Text(camera.results.isEmpty ? "No Input" : camera.results.joined(separator: ", "))
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
         }
         .edgesIgnoringSafeArea(.all)
         .onAppear {
