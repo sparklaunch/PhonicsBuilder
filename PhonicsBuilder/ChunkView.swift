396dd032e50956c8cc0755fec56068f13a1cd0a6
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-13 14:05:49 +0900

    [Add] Chunk bounce animation.

--- PhonicsBuilder/ChunkView.swift
+++ PhonicsBuilder/ChunkView.swift
@@ -1,10 +1,22 @@
 import SwiftUI
 
 struct ChunkView: View {
+    @State private var chunkScale = 1.0
     let text: String
     var body: some View {
-        Text(text)
-            .font(.custom("Poppins", size: 150, relativeTo: .largeTitle))
-            .frame(maxWidth: .infinity, alignment: .center)
+        ZStack {
+            Text(text)
+                .font(.custom("Poppins", size: 150, relativeTo: .largeTitle))
+                .scaleEffect(chunkScale)
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 0.25)) {
+                chunkScale = 1.5
+            }
+            withAnimation(.easeInOut(duration: 0.25).delay(0.25)) {
+                chunkScale = 1.0
+            }
+        }
     }
 }
