13bb520d01958f0f3371e52efb407110029675af
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-13 16:44:16 +0900

    [Refactor] UIImageExtensions.

--- PhonicsBuilder/Camera.swift
+++ PhonicsBuilder/Camera.swift
@@ -223,21 +223,3 @@ struct Preview: UIViewRepresentable {
         // TODO: NOTHING TO DO.
     }
 }
-
-extension UIImage {
-    func rotate(radians: CGFloat) -> UIImage {
-        let rotatedSize = CGRect(origin: .zero, size: size)
-            .applying(CGAffineTransform(rotationAngle: CGFloat(radians)))
-            .integral.size
-        UIGraphicsBeginImageContext(rotatedSize)
-        if let context = UIGraphicsGetCurrentContext() {
-            let origin = CGPoint(x: rotatedSize.width / 2.0, y: rotatedSize.height / 2.0)
-            context.translateBy(x: origin.x, y: origin.y)
-            context.rotate(by: radians)
-            draw(in: CGRect(x: -origin.y, y: -origin.x, width: size.width, height: size.height))
-            let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
-            return rotatedImage ?? self
-        }
-        return self
-    }
-}
