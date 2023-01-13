a17d97486ffe8160a68cf8dd943f77caeb0d31b1
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-13 16:37:41 +0900

    [Refactor] ViewExtensions.

--- PhonicsBuilder/EdgeBorder.swift
+++ PhonicsBuilder/EdgeBorder.swift
@@ -43,9 +43,3 @@ struct EdgeBorder: Shape {
         return path
     }
 }
-
-extension View {
-    func border(width: CGFloat, edges: [Edge], color: Color) -> some View {
-        overlay(EdgeBorder(width: width, edges: edges).foregroundColor(color))
-    }
-}
---
+++ PhonicsBuilder/ViewExtensions.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+extension View {
+    func border(width: CGFloat, edges: [Edge], color: Color) -> some View {
+        overlay(EdgeBorder(width: width, edges: edges).foregroundColor(color))
+    }
+}
