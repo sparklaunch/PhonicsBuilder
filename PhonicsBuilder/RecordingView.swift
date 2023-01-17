d4f0184446937b23630bce18ca5bd13cd3f34d86
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-17 19:37:49 +0900

    [Add] Animated Speak Now text.

--- PhonicsBuilder/RecordingView.swift
+++ PhonicsBuilder/RecordingView.swift
@@ -1,9 +1,22 @@
 import SwiftUI
 
 struct RecordingView: View {
+    @State private var bouncing = false
     var body: some View {
         ZStack {
             Color.white.opacity(0.8)
+            VStack {
+                Text("Speak Now")
+                    .font(.custom("Poppins", size: 64))
+                    .foregroundColor(Color("MainColor"))
+                    .padding()
+                    .scaleEffect(bouncing ? 1.0 : 1.5)
+                    .animation(.linear(duration: 0.5).repeatForever(autoreverses: true), value: bouncing)
+                    .onAppear {
+                        bouncing = true
+                    }
+                Spacer()
+            }
         }
     }
 }
