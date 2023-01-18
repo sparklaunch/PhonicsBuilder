0a08393364b1be83d46f802aed572cfb17d47ba0
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-19 02:08:18 +0900

    [Update] ExcellentText animation.

--- PhonicsBuilder/ExcellentText.swift
+++ PhonicsBuilder/ExcellentText.swift
@@ -8,7 +8,7 @@ struct ExcellentText: View {
             .foregroundColor(Color("MainColor"))
             .padding()
             .scaleEffect(bouncing ? 1.0 : 1.2)
-            .animation(.linear(duration: 0.5).repeatForever(autoreverses: true), value: bouncing)
+            .animation(.interactiveSpring(response: 0.5, dampingFraction: 1, blendDuration: 0.25).repeatForever(autoreverses: true), value: bouncing)
             .onAppear {
                 bouncing = true
             }
