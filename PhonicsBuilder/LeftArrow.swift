21fb4817029550931d166ad7c322f96afed2d19b
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-18 09:50:14 +0900

    [Fix] Left arrow function.

--- PhonicsBuilder/LeftArrow.swift
+++ PhonicsBuilder/LeftArrow.swift
@@ -1,12 +1,19 @@
 import SwiftUI
 
 struct LeftArrow: View {
+    @EnvironmentObject private var globalState: GlobalState
     var body: some View {
         VStack {
             HStack {
                 LottieView(jsonName: "Left")
                     .frame(width: 100, height: 100)
                     .padding()
+                    .onTapGesture {
+                        withAnimation {
+                            globalState.showingResultsScreen = false
+                            globalState.isRecording = false
+                        }
+                    }
                 Spacer()
             }
             Spacer()
