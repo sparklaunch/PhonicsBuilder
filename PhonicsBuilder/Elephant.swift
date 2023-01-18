9f62d330ff82677a6f5e3b32cb9f7328ddec9fd6
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-19 01:54:55 +0900

    [Update] Elephant's position.

--- PhonicsBuilder/Elephant.swift
+++ PhonicsBuilder/Elephant.swift
@@ -2,14 +2,16 @@ import SwiftUI
 
 struct Elephant: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             Spacer()
-            HStack {
+            HStack(spacing: 0) {
                 Spacer()
                 Image("Elephant")
                     .scaleEffect(2.0)
                     .padding(100)
+                    .offset(y: 100)
             }
+            Spacer()
         }
     }
 }
