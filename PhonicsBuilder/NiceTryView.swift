faeaaafc1097cddc5d742bbc73ac70bd6a30132c
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-19 10:00:10 +0900

    [Complete] NiceTry Screen.

--- PhonicsBuilder/NiceTryView.swift
+++ PhonicsBuilder/NiceTryView.swift
@@ -24,7 +24,7 @@ struct NiceTryView: View {
             Color("NiceTryColor")
             NiceTryBackground()
             VStack {
-                GoodText()
+                NiceTryText()
                 Spacer()
                     .frame(height: 100)
                 ChunksContainerView()
