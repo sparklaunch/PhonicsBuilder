4c9040f1fb23daa4db303f6a130ee9827bfdc063
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-19 15:15:22 +0900

    [Update] 결과 타이틀 텍스트 패딩 수정.

--- PhonicsBuilder/GoodText.swift
+++ PhonicsBuilder/GoodText.swift
@@ -4,8 +4,8 @@ struct GoodText: View {
     @State private var bouncing = false
     var body: some View {
         Image("Good")
-            .scaleEffect(bouncing ? 1.0 : 1.2)
-            .padding()
+            .scaleEffect(bouncing ? 1.2 : 1.5)
+            .padding(.top, 50)
             .animation(.interactiveSpring(response: 0.5, dampingFraction: 1, blendDuration: 0.25).repeatForever(autoreverses: true), value: bouncing)
             .onAppear {
                 bouncing = true
--- PhonicsBuilder/NiceTryText.swift
+++ PhonicsBuilder/NiceTryText.swift
@@ -3,11 +3,9 @@ import SwiftUI
 struct NiceTryText: View {
     @State private var bouncing = false
     var body: some View {
-        Text("Nice Try")
-            .font(.custom("EFUTURE4_EB", size: 100))
-            .foregroundColor(Color("MainColor"))
+        Image("NiceTry")
+            .scaleEffect(bouncing ? 1.2 : 1.5)
             .padding(.top, 50)
-            .scaleEffect(bouncing ? 1.0 : 1.2)
             .animation(.interactiveSpring(response: 0.5, dampingFraction: 1, blendDuration: 0.25).repeatForever(autoreverses: true), value: bouncing)
             .onAppear {
                 bouncing = true
