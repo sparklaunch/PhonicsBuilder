a3b066f51c4d969f16e6d8a488273133a265ea98
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-19 15:11:22 +0900

    [Update] GoodText 이미지로 수정.

--- PhonicsBuilder/GoodText.swift
+++ PhonicsBuilder/GoodText.swift
@@ -3,11 +3,9 @@ import SwiftUI
 struct GoodText: View {
     @State private var bouncing = false
     var body: some View {
-        Text("Good")
-            .font(.custom("EFUTURE4_EB", size: 100))
-            .foregroundColor(Color("MainColor"))
-            .padding()
+        Image("Good")
             .scaleEffect(bouncing ? 1.0 : 1.2)
+            .padding()
             .animation(.interactiveSpring(response: 0.5, dampingFraction: 1, blendDuration: 0.25).repeatForever(autoreverses: true), value: bouncing)
             .onAppear {
                 bouncing = true
